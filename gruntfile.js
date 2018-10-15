3bb8269360fe1f2b0b3849ef6fec48ab7621d7bf
evasutkova <evka.skulova@gmail.com>
2018-10-15 10:35:48 +0200

    Zaklad grunt file

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+    //#region [ Config ]
+
+    grunt.initConfig({
+        package: grunt.file.readJSON("package.json")
+    });
+
+    //#endregion
+
+
+    //#region [ Tasks ]
+    //#endregion
+
+
+    //#region [ Methods ]
+    //#endregion
+};

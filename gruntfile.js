63e7aa6b85fd4aade6886db0f42768d0f21bd82d
evasutkova <evka.skulova@gmail.com>
2018-10-15 10:38:46 +0200

    Clean task

--- gruntfile.js
+++ gruntfile.js
@@ -2,16 +2,36 @@ module.exports = function (grunt) {
     //#region [ Config ]
 
     grunt.initConfig({
-        package: grunt.file.readJSON("package.json")
+        package: grunt.file.readJSON("package.json"),
+        clean: {
+            wwwroot: [
+                "wwwroot/**/*"
+            ]
+        }        
     });
 
     //#endregion
 
 
     //#region [ Tasks ]
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.registerTask("cleanTask", cleanTask);
+
     //#endregion
 
 
     //#region [ Methods ]
+
+    /**
+     * Clean task.
+     */
+    function cleanTask() {
+        grunt.task.run.apply(grunt.task, [
+            "clean:wwwroot"
+        ]);
+    }
+
     //#endregion
 };

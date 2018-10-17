7e2817d6d553a7983b75bf8a718dea321d1232f6
evasutkova <evka.skulova@gmail.com>
2018-10-17 12:37:20 +0200

    Registtracia user komponentu

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ define([
     // Registracia komponentov
     ko.components.register("app", { require: "dp/components/app/app" });
     ko.components.register("toolbar", { require: "dp/components/toolbar/toolbar" });
+    ko.components.register("user", { require: "dp/components/user/user" });
     ko.components.register("drive-tool", { require: "dp/components/tools/drive/drive" });
     ko.components.register("explorer-tool", { require: "dp/components/tools/explorer/explorer" });
     ko.components.register("settings-tool", { require: "dp/components/tools/settings/settings" });

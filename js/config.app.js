77b3ad86b28d2ce6c2aa946e0e27a44808c6f586
evasutkova <evka.skulova@gmail.com>
2018-10-17 07:42:40 +0200

    Konfiguracia pre pripojenie na google drive

--- js/config.app.js
+++ js/config.app.js
@@ -13,7 +13,18 @@ require({
         google: "/dp/wwwroot/js/libs/google"
     },
     config: {
-        "dp/components/app/app": {
+        "dp/components/tools/drive/drive": {
+            apiKey: "AIzaSyDNFM5eXTFSr9V1CkmKlIx_5rlC7_b2xdY",
+            clientId: "132615756619-dr6kdei3tegn175epmadh6043r70f60p.apps.googleusercontent.com",
+            discoveryDocs: [
+                "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
+            ],
+            scopes: [
+                "https://www.googleapis.com/auth/drive.metadata.readonly",
+                "https://www.googleapis.com/auth/drive", 
+                "https://www.googleapis.com/auth/drive.file", 
+                "https://www.googleapis.com/auth/drive.readonly"
+            ]            
         }
     }
 }, ["dp/main"]);

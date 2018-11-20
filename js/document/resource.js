813668d122701a019bedf8f1f8157daee05650d3
evasutkova <evka.skulova@gmail.com>
2018-11-20 09:35:50 +0100

    pri serializacii do json posielat s resourcom aj serach

--- js/document/resource.js
+++ js/document/resource.js
@@ -43,7 +43,8 @@ define([
     Model.prototype.toJson = function() {
         var n = {
             title: this.title(),
-            url: this.url()
+            url: this.url(),
+            search: this.search()
         };
 
         var tmp = this.isActive();
